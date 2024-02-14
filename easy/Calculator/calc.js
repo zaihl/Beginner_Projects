8f9fe8b7a39a1c20d34074d18dcaa1c2dc8b61d4
Sahil Ingole <109749616+zaihl@users.noreply.github.com>
2024-02-14 09:11:29 +0530

    Added Calculator project

---
+++ easy/Calculator/calc.js
@@ -0,0 +1,19 @@
+const display = document.getElementById(`display`);
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay () {
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);  // evaluates the string with operators.
+    } catch(error) {
+        display.value = "Error";
+    }
+
+}
+
